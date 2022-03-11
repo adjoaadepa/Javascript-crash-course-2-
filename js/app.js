5881b714d185ccd927cdc9416966235e2fdcd8cf
adjoaadepa <osafoadjowashirley1234@gmail.com>
2022-03-11 15:37:51 +0000

    initial commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const randomNumber = () => {};
+function randomNumber() {}
+
+//
+let i = 25;
+let a = 10;
+
+function calc(i, a) {}
+
+const arr = [];
+let obj = {};
+calc();
+
+// function name(params) {
+//   function name(params) {}
+// }
+
+// menstrual212340
---
+++ js/script.js
@@ -0,0 +1,27 @@
+let firstName = "Emmanuel";
+let lastName = "Bans";
+let age = 21;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(
+  "my name is " + firstName + "" + lastName + "I am " + age + "years old"
+);
+console.log(`My name is ${firstName} ${lastName}. I am ${age} years old.`);
+
+let fruits = ["Mango", "Apple", "Orange", "Pineapple"];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[2]);
+console.log(fruits.push("banana"));
+console.log(fruits);
+console.log(fruits.pop());
+console.log(fruits.pop());
+console.log(fruits);
+console.log(fruits);
+console.log(fruits.unshift("pear"));
+console.log(fruits.shift());
+console.log(fruits);
+console.log(fruits.includes("ginger"));
+console.log(fruits.join("-"));
